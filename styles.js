18af4ecf28e66a3a13b17ba456a03914456bd276
Monji Dolon <mdolon@gmail.com>
2019-11-15 17:33:28 -0500

    Rename prop and remove absolute positioning

--- styles.js
+++ styles.js
@@ -2,21 +2,21 @@
 
 import { StyleSheet } from 'react-native';
 
-const absoluteStretch = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  bottom: 0,
-  right: 0,
-};
+// const absoluteStretch = {
+//   position: 'absolute',
+//   top: 0,
+//   left: 0,
+//   bottom: 0,
+//   right: 0,
+// };
 
 export default StyleSheet.create({
   container: {
-    ...absoluteStretch,
+    // ...absoluteStretch,
     justifyContent: 'center',
   },
   menu: {
-    ...absoluteStretch,
+    // ...absoluteStretch,
   },
   frontView: {
     flex: 1,
@@ -27,7 +27,7 @@ export default StyleSheet.create({
     overflow: 'hidden',
   },
   overlay: {
-    ...absoluteStretch,
+    // ...absoluteStretch,
     backgroundColor: 'transparent',
   },
 });
